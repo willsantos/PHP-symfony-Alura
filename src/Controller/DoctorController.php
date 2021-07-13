54d53e3b891bee9520bd27870964502ecc826730
Wilson Santos <contato@wilsonsantos.net>
2021-07-12 23:31:55 -0300

    :sparkles: fim do capitulo 3
    Integração do doctrine, migrations, inserção no banco e buscas com diferentes rotas

--- src/Controller/DoctorController.php
+++ src/Controller/DoctorController.php
@@ -2,13 +2,24 @@
 namespace App\Controller;
 
 use App\Entity\Doctor;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
-class DoctorController
+class DoctorController extends AbstractController
 {
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager )
+    {
+       $this->entityManager = $entityManager;
+    }
     /**
      * @Route("/medicos", methods={"POST"})
      */
@@ -22,6 +33,38 @@ class DoctorController
         $doctor->crm = $bodyJson->crm;
         $doctor->name = $bodyJson->name;
 
+        $this->entityManager->persist($doctor);
+        $this->entityManager->flush();
+
         return new JsonResponse($doctor);
     }
+
+    /**
+     * @Route("/medicos",methods={"GET"})
+     */
+
+    public function findAll():Response
+    {
+        $doctorRepository = $this
+            ->getDoctrine()
+            ->getRepository(Doctor::class);
+
+        $doctorList = $doctorRepository->findAll();
+        return new JsonResponse($doctorList);
+    }
+
+    /**
+     * @Route("/medicos/{id}",methods={"GET"})
+     */
+    public function findByOne(Request $request): Response
+    {
+        $id = $request->get('id');
+        $doctorRepository = $this
+            ->getDoctrine()
+            ->getRepository(Doctor::class);
+        $doctor = $doctorRepository->find($id);
+        $statusCode = is_null($doctor) ? Response::HTTP_NO_CONTENT : 200;
+        return new JsonResponse($doctor,$statusCode);
+    }
+
 }
--- src/Entity/Doctor.php
+++ src/Entity/Doctor.php
@@ -1,9 +1,25 @@
 <?php
 namespace App\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+/**
+ * @ORM\Entity()
+ */
 class Doctor
 {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    public $id;
+    /**
+     * @ORM\Column(type="integer")
+     */
     public $crm;
+    /**
+     * @ORM\Column(type="string")
+     */
     public $name;
 
 
